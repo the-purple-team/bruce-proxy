0dd3dfe13aa55628331b03670c84931307f71f20
Bruce <bruce@yogainternational.com>
2019-06-13 22:52:54 -0400

    adds script to start server adds server-side code

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
 const express = require('express');
+const app = express();
+
+const port = '5555';
+
+app.use(express.static(__dirname + '/index.html'));
+
+app.get('/', () => {
+  res;
+});
+
+app.listen(port, () => {
+  console.log(`App hosted on port ${port}`);
+});
